756a3db441caa0858b5ce6cb52de7892d5e3e9e5
Jackson Mohsenin <jmohsenin@gmail.com>
2020-10-26 18:34:09 -0700

    Add 2020-10-10 pizza, spellcheck previous entries

--- pizza_images.sh
+++ pizza_images.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SIZE=250
 RETINA_SIZE=$((SIZE * 2))
+EXPORT_PATH=/Users/jmo/jmohsenin.github.io/assets/images/pizzas/
 
 for filename in /Users/jmo/Documents/2020-\ Freelance/pizzas-raw/*; do
     # path/to/filename.jpg -> filename.jpg
@@ -8,8 +9,8 @@ for filename in /Users/jmo/Documents/2020-\ Freelance/pizzas-raw/*; do
     # filename.jpg -> filename
     y=${x%.*}
 
-    magick "$filename" -resize $SIZEx$SIZE -quality 70 /Users/jmo/jmohsenin.github.io/assets/images/pizzas/${y}.jpg
-    magick "$filename" -resize $RETINA_SIZEx$RETINA_SIZE -quality 70 /Users/jmo/jmohsenin.github.io/assets/images/pizzas/${y}@2x.jpg
-    magick "$filename" -resize $SIZEx$SIZE -quality 70 -define webp:lossless=false /Users/jmo/jmohsenin.github.io/assets/images/pizzas/${y}.webp
-    magick "$filename" -resize $RETINA_SIZEx$RETINA_SIZE -quality 70 -define webp:lossless=false /Users/jmo/jmohsenin.github.io/assets/images/pizzas/${y}@2x.webp
+    magick "$filename" -resize $SIZEx$SIZE -quality 70 $EXPORT_PATH${y}.jpg
+    magick "$filename" -resize $RETINA_SIZEx$RETINA_SIZE -quality 70 $EXPORT_PATH${y}@2x.jpg
+    magick "$filename" -resize $SIZEx$SIZE -quality 70 -define webp:lossless=false $EXPORT_PATH${y}.webp
+    magick "$filename" -resize $RETINA_SIZEx$RETINA_SIZE -quality 70 -define webp:lossless=false $EXPORT_PATH${y}@2x.webp
 done
